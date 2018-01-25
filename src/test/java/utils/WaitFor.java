cd6fa5bab151d80d3a5876b69851031a04848c02
michaldz1 <michaldz1@poczta.onet.pl>
2018-01-25 23:17:31 +0100

    It works

--- src/test/java/utils/WaitFor.java
+++ src/test/java/utils/WaitFor.java
@@ -7,20 +7,16 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class WaitFor {
     private static WebDriverWait wait;
-//
-//    public static void webElementVisible(By byValue) {
-//        wait = new WebDriverWait(DriverManager.getDriver(), 10);
-//        wait.until(ExpectedConditions.visibilityOfElementLocated(byValue));
-//    }
+
     public static void webElementVisible(WebElement webelement) {
         wait = new WebDriverWait(DriverManager.getDriver(), 10);
         wait.until(ExpectedConditions.visibilityOf(webelement));
     }
 
 
-    public static void webElementVisible(By byValue, int timeout) {
+    public static void webElementVisible(WebElement webelement, int timeout) {
         wait = new WebDriverWait(DriverManager.getDriver(), timeout);
-        wait.until(ExpectedConditions.visibilityOfElementLocated(byValue));
+        wait.until(ExpectedConditions.visibilityOf(webelement));
     }
 
 
