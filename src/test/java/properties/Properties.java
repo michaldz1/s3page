b4b49fb1c193c19ae06299f9901d935ab8993840
michaldz1 <michaldz1@poczta.onet.pl>
2018-02-01 23:27:47 +0100

    It works

--- src/test/java/properties/Properties.java
+++ src/test/java/properties/Properties.java
@@ -6,11 +6,43 @@ import org.openqa.selenium.By;
  * Created by michald on 23/01/2018.
  */
 public class Properties {
-    public static final String S3_URL = "https://www.s3group.com/";
-    public static final String EXPECTED_STRING_ERROR = "There was a problem with your submission. Errors have been highlighted below.";
-    public static final String GECKO_PATH = "C:\\Users\\michald\\michalSelenium\\geckodriver.exe";
-    public static final String CHROMEDRIVER_PATH = "C:\\Users\\michald\\michalSelenium\\chromedriver.exe";
-    public static final By VALIDATION_ERROR = By.className("validation_error");
-    public static String someText = "This is some unimportant text";
-    public static String browserType = "Firefox";
+    public static final String S3_URL="https://www.s3group.com/";
+    public static final String EXPECTED_STRING_ERROR="There was a problem with your submission. Errors have been highlighted below.";
+    public static final String GECKO_PATH="C:\\Users\\michald\\michalSelenium\\geckodriver.exe";
+    public static final String CHROMEDRIVER_PATH="C:\\Users\\michald\\michalSelenium\\chromedriver.exe";
+    public static final By VALIDATION_ERROR=By.className("validation_error");
+    public static String someText="This is some unimportant text";
+    public static String browserType="Firefox";
+
+    SELECTORS:
+    ourBusiness = #menu-item-1098
+    s3Logo =.navbar-brand
+    ourBusiness_s3Semiconductors = #menu-item-778
+    ourBusiness_connectedHealth = #menu-item-779
+    careers = #menu-item-401
+    careers_vacancies = #menu-item-444
+    careers_meetOurPeople = #menu-item-402
+    careers_ourRecruitmentProces = #menu-item-404
+    careers_fAQ = #menu-item-405
+    careers_reqruitmentAgencies = #menu-item-403
+    careers_alumni = #menu-item-909
+    newsAndEvents = #menu-item-645
+    about = #menu-item-653
+    about_introduction = #menu-item-649
+    about_customers = #menu-item-644
+    about_quality = #menu-item-409
+    about_locations = #menu-item-635
+    about_leadershipTeam = #menu-item-408
+    about_investors = #menu-item-407
+    contactUs = .btn btn-red btn-contact
+    semiconductorsBanner = ??
+    connectedHealthBanner = ??
+    playMovie = .icon-s3-icon-play
+    moreButton = ??// contains("More")
+    sitemap = #menu-item-990
+    legal = #menu-item-1327
+    privacy = #menu-item-1328
+
+
+
 }
