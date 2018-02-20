a88ef876a133f54d5dc0de84d3917b4f5434c636
michaldz1 <michaldz1@poczta.onet.pl>
2018-02-20 22:10:00 +0100

    testForElementsOnPage

--- src/test/java/customWebElement/DropdownListWebCom.java
+++ src/test/java/customWebElement/DropdownListWebCom.java
@@ -3,6 +3,7 @@ package customWebElement;
 import com.github.webdriverextensions.WebComponent;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.Select;
 
 /**
  * Created by michald on 20/02/2018.
@@ -11,4 +12,8 @@ public class DropdownListWebCom extends WebComponent {
     @FindBy(className = "form-control")
     public WebElement dropDownListWebCom;
 
+    public static void SelectFromDropDownList(DropdownListWebCom elementList, int numberOnList){
+        Select select = new Select(elementList);
+        select.selectByIndex(numberOnList);
+    }
 }
--- src/test/java/page/CareersPage.java
+++ src/test/java/page/CareersPage.java
@@ -1,24 +1,13 @@
 package page;
 
-//import customWebElement.DropdownList;
-//import org.openqa.selenium.WebDriver;
 import com.github.webdriverextensions.WebDriverExtensionFieldDecorator;
 import customWebElement.DropdownListWebCom;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.PageFactory;
-//import org.openqa.selenium.WebElement;
-//import org.openqa.selenium.remote.RemoteWebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.ui.Select;
 import utils.DriverManager;
-//import utils.WaitFor;
-//import utils.Assertion;
-//import properties.Properties;
-//import org.openqa.selenium.support.PageFactory;
-//import org.openqa.selenium.support.ui.Select;
-import com.github.webdriverextensions.WebComponent;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.FindBy;
+
 
 /**
  * Created by michald on 18/02/2018.
@@ -34,8 +23,9 @@ public class CareersPage {
     private DropdownListWebCom dropdownListWebCom;
 
     public page.CareersPage selectBusinessUnit(int dropdownListIndex) {
-        Select select = new Select(dropdownListWebCom);
-        select.selectByIndex(dropdownListIndex);
+//        Select select = new Select(dropdownListWebCom);
+//        select.selectByIndex(dropdownListIndex);
+        DropdownListWebCom.SelectFromDropDownList( dropdownListWebCom,dropdownListIndex );
         return this;
     }
 
